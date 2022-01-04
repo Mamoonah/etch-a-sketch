d6f6b9c5dbc5b17ac4d1a7735e22e4fc98508e80
Sebastian Rachwał <sebuh@tlen.pl>
2022-01-04 21:48:20 +0100

    - add Rainbow Mode - add reset button

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 let paintArea = document.querySelector('#grid');
-let slider = document.querySelector('#squares-quantity')
+let slider = document.querySelector('#squares-quantity');
+let resetButton = document.querySelector('#reset');
+let rainbow = document.getElementById('rainbow');
+
+let rainbowMode;
+
+rainbow.addEventListener('click', () => {
+    rainbowMode = rainbow.checked;
+    sizeUpdate();
+})
 
 slider.addEventListener('change', sizeUpdate)
+resetButton.addEventListener('click', sizeUpdate)
 
 sizeUpdate()
 
@@ -23,7 +33,17 @@ function sizeUpdate(){
 function createSquares(parrent, quantity){
     for(let i = 0; i < quantity**2; i++){
         let square = document.createElement('div');
+        square.addEventListener('mouseover', changeColor)
         square.classList.add('square')
         parrent.appendChild(square)
     }
+}
+
+function changeColor(e){
+    if(rainbowMode){
+        e.target.style['background-color'] = `rgba(${Math.random()*200}, ${Math.random()*200}, ${Math.random()*200}, 1)`;
+    } else {
+        e.target.style['background-color'] = 'rgba(0, 0, 0, 0.7)'
+    }
+    e.target.removeEventListener('mouseover', changeColor);
 }
